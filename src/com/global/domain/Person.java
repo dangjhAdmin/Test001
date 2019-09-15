a59228af8c819ee6947f02636eb07f4a6b66fccf
dangjunhui <616172218@qq.com>
2019-09-16 07:01:35 +0800

    Person.java

--- src/com/global/domain/Person.java
+++ src/com/global/domain/Person.java
@@ -6,18 +6,18 @@ package com.global.domain;
  * 实体类
  */
 public class Person {
-    private String id;
-    private String name;
-    private String age;
-    private String certNo;
-    private String certType;
-    private String bancsNo;
-    private String custUid;
-    private String phone;
-    private String address;
-    private String seqEmp;
-    private String seqOrgId;
-    private String seqOrgCode;
+    private String id;//ID
+    private String cnname;// 中文姓名
+    private String age;// 英文姓名
+    private String certNo;//證件號碼
+    private String certType;// 證件類型
+    private String bancsNo;// 核心客戶號
+    private String custUid;//客户UID
+    private String phone;// 手機號碼
+    private String address;// 地址
+    private String seqEmp;// 所屬客戶經理
+    private String seqOrgId;// 機構ID
+    private String gender;// 性別
 
 
 }
